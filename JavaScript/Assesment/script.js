4f6794f4e022da75e008091e0645cd9656170983
José Ricardo Lima dos Santos Pereira <jricardo@trustprev.com.br>
2020-06-17 03:57:02 -0300

    AJUSTE PART 3

--- JavaScript/Assesment/script.js
+++ JavaScript/Assesment/script.js
@@ -1,11 +1,12 @@
 let $form = document.querySelector('.js_form');
-let $retorno = document.querySelector('.js_msg');
 let $select = document.querySelector('.js_combo_moedas');
 let $input = document.querySelector('.js_quantidade');
 let $table = $form.querySelector('.table');
 let $tbody = $table.querySelector('tbody');
+let $date_select = document.querySelector('.js_dateTime');
+let $input_limpar = document.querySelector('.js_limpar');
+let $div_relatorio = document.querySelector('.relatorio');
 let date_now = new Date().toString();
-let date_select;
 let coin = [];
 let historico = []
 
@@ -17,14 +18,52 @@ carregaHistoricoTabela();
 
 $form.addEventListener('submit', (e) => {
     e.preventDefault();
+    let vlQnt = $input.value;
+    let vlMoeda = $select.value;
+    let vlData = $date_select.value;
 
-    date_select = $form.querySelector('input[type=date]').value
-    if (!date_select.trim()) {
-        window.alert("preencha o campo !")
+    if (!vlQnt.trim() && !vlMoeda.trim() && !vlData.trim()) {
+        $input.classList.add('is-invalid');
+        $select.classList.add('is-invalid');
+        $date_select.classList.add('is-invalid');
     } else {
-        filtrar();
+        if (!vlQnt.trim()) {
+            window.alert('Preencha o campo: ' + $input.name + ' !');
+            $input.classList.add('is-invalid');
+            $input.focus();
+        } else {
+            $input.classList.remove('is-invalid');
+            if (!vlMoeda.trim()) {
+                window.alert('Preencha o campo: ' + $select.name + ' !');
+                $select.classList.add('is-invalid');
+                $select.focus();
+            } else {
+                $select.classList.remove('is-invalid');
+                if (!vlData.trim()) {
+                    window.alert('Preencha o campo: ' + $date_select.name + ' !');
+                    $date_select.classList.add('is-invalid');
+                    $date_select.focus();
+                } else {
+                    $date_select.classList.remove('is-invalid');
+                    filtrar()
+                    $input_limpar.classList.remove('d-none');
+                }
+            }
+        }
     }
 })
+
+$input_limpar.addEventListener('click', (e) => {
+    e.preventDefault();
+    localStorage.removeItem('Historico');
+    while ($tbody.hasChildNodes()) {
+        $tbody.removeChild($tbody.firstChild);
+    }
+    while ($div_relatorio.hasChildNodes()) {
+        $div_relatorio.removeChild($div_relatorio.firstChild);
+    }
+    $input_limpar.classList.add('d-none');
+});
 
 
 async function filtrar() {
@@ -36,7 +75,7 @@ async function filtrar() {
     let valorHoje
 
     try {
-        apiDataSelecionado = await getValorDataSelecionada(date_select, $select.value);
+        apiDataSelecionado = await getValorDataSelecionada($date_select.value, $select.value);
         valorDataSelecionada = apiDataSelecionado.rates["BRL"];
 
         apiDataAtual = await getValorDataHoje($select.value);
@@ -51,7 +90,7 @@ async function filtrar() {
         criarEl('td', $select.value, $tr, {
             className: 'text-center'
         });
-        criarEl('td', formatDateCampo(date_select), $tr, {
+        criarEl('td', formatDateCampo($date_select.value), $tr, {
             className: 'text-center'
         });
         criarEl('td', valorDataSelecionada.toFixed(2), $tr, {
@@ -66,22 +105,31 @@ async function filtrar() {
         }), $tr, {
             className: 'text-center'
         });
-    
+
         criarButton($tr);
-    
-        historico.push(new registro($input.value, $select.value, formatDateCampo(date_select), valorDataSelecionada.toFixed(2), valorCotacao.toFixed(2), valorHoje.toLocaleString('pt-br', {
+
+        historico.push(new registro($input.value, $select.value, formatDateCampo($date_select.value), valorDataSelecionada.toFixed(2), valorCotacao.toFixed(2), valorHoje.toLocaleString('pt-br', {
             style: 'currency',
             currency: 'BRL'
         })))
-    
+
         let serializado = JSON.stringify(historico)
         localStorage.setItem(`Historico`, serializado);
         console.log('Salvo com sucesso');
+
+        criarRelatorio($select.value, valorCotacao.toFixed(2), valorHoje.toLocaleString('pt-br', {
+            style: 'currency',
+            currency: 'BRL'
+        }), $div_relatorio);
+
+        $input.value = '';
+        $date_select.value = '';
+        $select.value = '';
+
+    } catch (ex) {
+        window.alert('ERRO: ' + ex)
     }
-    catch(ex){
-        window.alert('ERRO:' + ex)
-    }
-   
+
 
 }
 
@@ -137,7 +185,7 @@ function carregaComboMoeda() {
 function carregaHistoricoTabela() {
 
     let cache = localStorage.getItem(`Historico`);
-    if (cache != null || !cache.trim()) {
+    if (cache != null) {
         cache = JSON.parse(cache)
         for (let items of cache) {
             let $tr = criarEl('tr', null, $tbody);
@@ -148,6 +196,7 @@ function carregaHistoricoTabela() {
             }
             criarButton($tr);
         }
+        $input_limpar.classList.remove('d-none');
     }
 }
 
@@ -195,16 +244,35 @@ function vPercentual(n1, n2) {
 function criarEl(tipo, texto, $pai, extra) {
     let $el = document.createElement(tipo);
     if (texto) {
-        $el.innerText = texto
+        $el.innerText = texto;
     }
     if ($pai) {
-        $pai.append($el)
+        $pai.append($el);
     }
 
     for (let chave in extra) {
         $el[chave] = extra[chave]
     }
     return $el
+}
+
+function criarRelatorio(moeda, cota, somatorio, $pai) {
+    let $card = document.createElement('div');
+    let $card_body = document.createElement('div');
+    let $moeda = document.createElement('p');
+    let $cota = document.createElement('p');
+    let $somatorio = document.createElement('p');
+    $card.setAttribute("class", 'card col m-3');
+    $card_body.setAttribute("class", 'card-body');
+    if ($pai) {
+        $moeda.innerText = moeda;
+        $cota.innerText = cota;
+        $somatorio.innerText = somatorio;
+        $card_body.append($moeda, $cota, $somatorio);
+        $card.append($card_body);
+        $pai.append($card);
+    }
+    return $card
 }
 
 function criarButton($tr) {
